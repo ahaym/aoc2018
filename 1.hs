993bdca6e1222175dc5c2ed381d0757c6e5322c7
mark <mah6@williams.edu>
2018-12-02 08:08:07 +0900

    remove some lines

--- 1.hs
+++ 1.hs
@@ -2,17 +2,14 @@ import qualified Data.Set as S
 
 main = do
     inp <- readFile "1.in"
-    let parsed = parse inp
+    let parsed = (read . removePlus) <$> lines inp
     print $ sum parsed
     print $ sol2 S.empty 0 [] parsed
 
 sol2 m n [] xs0 = sol2 m n xs0 xs0
-sol2 m n (x:xs) xs0 = let cur = n + x in 
-    if S.member (n + x) m 
-        then n + x
-        else sol2 (S.insert (n + x) m) (n + x) xs xs0
-
-parse xs = (read . removePlus) <$> lines xs
+sol2 m n (x:xs) xs0 = if S.member (n + x) m 
+    then n + x
+    else sol2 (S.insert (n + x) m) (n + x) xs xs0
 
 removePlus ('+':xs) = xs
 removePlus xs = xs
